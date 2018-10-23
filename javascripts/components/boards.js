b67943338320808bfc0ef9bf95ea129ca9ad0696
mohammad <khezri@outlook.com>
2018-10-22 21:24:10 -0500

    ss

--- javascripts/components/boards.js
+++ javascripts/components/boards.js
@@ -1,18 +1,19 @@
-import {loadBoards} from '../data/boardsData.js';
+import { loadBoards } from '../data/boardsData.js';
+import { initialpinView } from './pins.js';
 
 const bindEvents = () => {
-  $('#user-boards').on('click', '.board-card', (e) => {
-    const clickedBoardId = $(e.target).closest('.board-card').attr('id');
-    $('#boards-page').hide();
-    $('#pins-page').show();
-    console.log(clickedBoardId);
-  })
-}
+	$('#user-boards').on('click', '.board-card', (e) => {
+		const clickedBoardId = $(e.target).closest('.board-card').attr('id');
+		$('#boards-page').hide();
+		$('#pins-page').show();
+		initialpinView();
+	});
+};
 
 const writeBoards = (boards) => {
-  let domString = '';
-  boards.forEach(board => {
-    domString += `
+	let domString = '';
+	boards.forEach((board) => {
+		domString += `
       <div id='${board.id}' class="board-card p-2">
         <img class="card-img-top" src="./db/default-img.jpeg" alt="Card image cap">
         <div class="card-body">
@@ -20,18 +21,20 @@ const writeBoards = (boards) => {
           <p class="card-text">42 Pins</p>
         </div>
       </div>
-      `
-  });
-  $('#user-boards').html(domString);
-}
+      `;
+	});
+	$('#user-boards').html(domString);
+};
 
 const initalizeBoardView = () => {
-  loadBoards().then((boards) => {
-    writeBoards(boards);
-    bindEvents();
-  }).catch((error) => {
-    console.error(error);
-  })
-}
+	loadBoards()
+		.then((boards) => {
+			writeBoards(boards);
+			bindEvents();
+		})
+		.catch((error) => {
+			console.error(error);
+		});
+};
 
-export {initalizeBoardView};
+export { initalizeBoardView };
